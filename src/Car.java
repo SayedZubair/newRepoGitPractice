4062a1961c8251a83b0ca966cfd176557bab7fef
SayedZubair <sayedzubairhashimi2020>
2022-12-18 13:02:42 -0800

    new feature added

--- src/Car.java
+++ src/Car.java
@@ -2,5 +2,8 @@ public class Car {
     public static void main(String[] args) {
         int speed;
         String color;
+        double price;
+        int model;
+        System.out.println("this feature has recently been added ");
     }
 }
